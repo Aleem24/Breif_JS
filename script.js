ae326322a20ae2330e4a47bb30ba836230d81304
Aleem24 <md.samiulaleem16@gmail.com>
2024-11-29 19:52:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const a= "";
+const b= "S";
+const c= 66;
+const d= "S";
+let e;
+
+document.write("B and C are equal: "+ (b===c));
+document.write("<br>");
+
+document.write("B and D are equal: " + (b === d));
+
+document.write("<br>");
+document.write("A=" + typeof a);
